7c2b77b3b5db197e6fec8efc285e93e6ded35ce3
Alyaman Maasarani <alyaman.x.p300@gmail.com>
2022-06-15 13:47:16 +0300

    switched to C++20 for modules support

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+
+// GLAD for OpenGL Extensions
 #include <glad/glad.h>
+// For drawing the window as a GLContext crossplatform
 #include <GLFW/glfw3.h>
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main()
-{
+int main() {
 
   glfwInit();
 
@@ -16,13 +18,12 @@ int main()
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(600, 400, "My Window", 0, 0);
-  if (window == NULL)
-    {
-      cout << "Failed to create GLFW Window" << endl;
-      glfwTerminate();
-      return -1;
-    }
+  GLFWwindow *window = glfwCreateWindow(600, 400, "My Window", 0, 0);
+  if (window == NULL) {
+    cout << "Failed to create GLFW Window" << endl;
+    glfwTerminate();
+    return -1;
+  }
   glfwMakeContextCurrent(window);
 
   gladLoadGL();
@@ -33,18 +34,16 @@ int main()
   glClear(GL_COLOR_BUFFER_BIT);
   glfwSwapBuffers(window);
 
-  do{
-      glClear( GL_COLOR_BUFFER_BIT );
+  do {
+    glClear(GL_COLOR_BUFFER_BIT);
 
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-    }
-  while(!glfwWindowShouldClose(window));
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  } while (!glfwWindowShouldClose(window));
 
   glfwDestroyWindow(window);
   glfwTerminate();
 
   cout << "Hello World!" << endl;
   return 0;
-
 }
