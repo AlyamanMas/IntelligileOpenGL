5ea1b1759a7ad2f6c6eec011c12c213b0ff98fe5
Alyaman Maasarani <alyaman.x.p300@gmail.com>
2022-06-27 14:04:07 +0300

    Working implementation of gdi::Color

--- src/gdi/Color.hpp
+++ src/gdi/Color.hpp
@@ -11,13 +11,19 @@ class ColorRGBA; // to solve circular dependency problem
 
 class ColorARGB {
 public:
+  // NOTE: despite the order of alpha and type here being alpha then
+  // type, the compiler starts ordering bitfield members from LSB to
+  // MSB, so putting alpha before type results in left most bit being
+  // type and the rest being alpha i.e. the default value for the
+  // first byte is 0x7F instead of 0xFE.
+
+  // default value 0x7F
+  unsigned int alpha : 7 {0b1111111};
   // 0 for True Type and 1 for indexed
   unsigned int type : 1;
-  // TODO: test
-  unsigned int alpha : 7 {0b1111111};
   uint8_t red, green, blue;
 
-  ColorARGB();
+  ColorARGB(){};
   ColorARGB(uint8_t red, uint8_t green, uint8_t blue);
   // Size of alpha will be reduced to 7 bits
   ColorARGB(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha_7bits);
@@ -30,12 +36,11 @@ public:
 class ColorRGBA {
 public:
   uint8_t red, green, blue;
+  unsigned int alpha : 7 {0b1111111};
   // 0 for True Type and 1 for indexed
   unsigned int type : 1;
-  // TODO: test
-  unsigned int alpha : 7 {0b1111111};
 
-  ColorRGBA();
+  ColorRGBA(){};
   ColorRGBA(uint8_t red, uint8_t green, uint8_t blue);
   // Size of alpha will be reduced to 7 bits
   ColorRGBA(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha_7bits);
