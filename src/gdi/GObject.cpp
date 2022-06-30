173d19450f6fbdcb2e84d746790e8908e9f749fd
Alyaman Maasarani <alyaman.x.p300@gmail.com>
2022-06-30 16:14:00 +0300

    Initial untested implementation of gdi::GObject

---
+++ src/gdi/GObject.cpp
@@ -0,0 +1,55 @@
+#include "GObject.hpp"
+#include "Drawable.hpp"
+#include "Point.hpp"
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+using namespace gdi;
+
+void GObject::set_x(float x) { this->x = x; };
+float GObject::get_x() const { return this->x; };
+void GObject::set_y(float y) { this->y = y; };
+float GObject::get_y() const { return this->y; };
+
+void GObject::insert_drawable(const shared_ptr<Drawable> drawable) {
+  this->content.push_back(drawable);
+};
+
+int GObject::delete_drawable(const shared_ptr<Drawable> drawable) {
+  try {
+
+    for (auto i = 0; i < this->content.size(); ++i) {
+      const auto &elem = this->content.at(i);
+
+      // equality test with raw pointer:
+      // if (elem.get() == drawable.get()) {
+      if (elem == drawable) {
+        this->content.erase(this->content.begin() + i);
+        return 0;
+      }
+    }
+    return 1;
+
+  } catch (...) {
+    return 1;
+  }
+};
+
+const vector<vector<GPoint>> GObject::get_drawables() const {
+  vector<vector<GPoint>> drawables{};
+  for (const auto &i : this->content) {
+    vector<GPoint> gpoints{i->provide_points()};
+    for (auto &j : gpoints) {
+      j.x += this->get_x();
+      j.y += this->get_y();
+    }
+    drawables.push_back(gpoints);
+  }
+
+  return drawables;
+};
+
+const vector<shared_ptr<Drawable>> &GObject::get_original_drawables() const {
+  return this->content;
+};
---
+++ src/gdi/GObject.hpp
@@ -0,0 +1,38 @@
+#ifndef GOBJECT_H_
+#define GOBJECT_H_
+
+#include "Drawable.hpp"
+#include "Point.hpp"
+
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+namespace gdi {
+class GObject {
+  vector<shared_ptr<Drawable>> content{};
+  float x, y;
+
+public:
+  GObject();
+  GObject(float x, float y) : x{x}, y{y} {};
+  virtual ~GObject(){};
+
+  void set_x(float x);
+  float get_x() const;
+  void set_y(float y);
+  float get_y() const;
+
+  void insert_drawable(const shared_ptr<Drawable> drawable);
+  /// Returns 0 if successful, 1 otherwise
+  int delete_drawable(const shared_ptr<Drawable> drawable);
+
+  /// Return drawables, adding GObject's x and y to their vertices
+  const vector<vector<GPoint>> get_drawables() const;
+  /// Return drawables without GObject's x and y
+  const vector<shared_ptr<Drawable>> &get_original_drawables() const;
+};
+} // namespace gdi
+
+#endif // GOBJECT_H_
